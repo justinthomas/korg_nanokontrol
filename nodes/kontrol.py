d8eef0ebee37b61febce066e00f2aab05d69ff89
Justin Thomas <jut@seas.upenn.edu>
2013-10-25 15:39:50 -0400

    Modified to ignore modes and use my current keymappings

--- nodes/kontrol.py
+++ nodes/kontrol.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 #
-# joystick input driver for Korg NanoKontrol input device
+# joystick input driver for Korg NanoKontrol2 input device
 #
 # Author: Austin Hendrix
+# Modified for NanoKontrol2 by: Justin Thomas
 
 import roslib; roslib.load_manifest('korg_nanokontrol')
 import rospy
@@ -15,53 +16,20 @@ import sys
 from sensor_msgs.msg import *
 
 control_axes = [{
+  # Syntax is channel: slide or knob number
   # mode 1, sliders
-   2:  0,  3:  1,  4:  2,  5:  3,  6:  4,  8:  5,  9:  6, 12:  7, 13:  8,
+   2:  0,  3:  1,  4:  2,  5:  3,  6:  4,  7:  5,  8:  6, 9:  7, 1:  8,
   # mode 1, knobs
-  14:  9, 15: 10, 16: 11, 17: 12, 18: 13, 19: 14, 20: 15, 21: 16, 22: 17,
-  },{
-  # mode 2, sliders
-  42:  0, 43:  1, 50:  2, 51:  3, 52:  4, 53:  5, 54:  6, 55:  7, 56:  8,
-  # mode 2, knobs
-  57:  9, 58: 10, 59: 11, 60: 12, 61: 13, 62: 14, 63: 15, 65: 16, 66: 17,
-  },{
-  # mode 3, sliders
-  85:  0, 86:  1, 87:  2, 88:  3, 89:  4, 90:  5, 91:  6, 92:  7, 93:  8,
-  # mode 3, knobs
-  94:  9, 95: 10, 96: 11, 97: 12, 102: 13, 103: 14, 104: 15, 105: 16, 106: 17,
-  },{
-  # mode 4, sliders
-  7: 0, 263: 1, 519: 2, 775: 3, 1031: 4, 1287: 5, 1543: 6, 1799: 7, 2055: 8,
-  # mode 4, knobs
-  10: 9, 266: 10, 522: 11, 778: 12, 1034: 13, 1290: 14, 1546: 15, 1802: 16,
-  2058: 17,
-  }]
+  10:  9, 11: 10, 12: 11, 13: 12, 14: 13, 15: 14, 16: 15}]
 
 control_buttons = [[
   # mode 1
   # up, down
-  23, 33, 24, 34, 25, 35, 26, 36, 27, 37, 28, 38, 29, 39, 30, 40, 31, 41,
-  # rew, play, ff, repeat, stop, rec
-  47, 45, 48, 49, 46, 44
-],[
-  # mode 2
-  # up, down
-  67, 76, 68, 77, 69, 78, 70, 79, 71, 80, 72, 81, 73, 82, 74, 83, 75, 84,
-  # rew, play, ff, repeat, stop, rec
-  47, 45, 48, 49, 46, 44
-],[
-  # mode 3
-  # up, down
-  107, 116, 108, 117, 109, 118, 110, 119, 111, 120, 112, 121, 113, 122, 114, 123, 115, 124,
-  # rew, play, ff, repeat, stop, rec
-  47, 45, 48, 49, 46, 44
-],[
-  # mode 4
-  # up, down
-  16, 17, 272, 273, 528, 529, 784, 785, 1040, 1041, 1296, 1297, 1552, 1553, 1808, 1809, 2064, 2065,
-  # rew, play, ff, repeat, stop, rec
-  47, 45, 48, 49, 46, 44
-]]
+  32, 33, 34, 35, 36, 37, 38, 39, 48, 49, 50, 51, 52, 53, 54, 55, 64, 65, 66, 67, 68, 69, 70, 71,
+  # rew, ff, stop, play, rec
+  43, 44, 42, 41, 45,
+  # Track L, Track R, Marker Set, Marker Left, Marker Right, Cycle
+  58, 59, 60, 61, 62, 46]]
 
 def main():
    pygame.midi.init()
@@ -86,8 +54,8 @@ def main():
    pub = rospy.Publisher('joy', Joy, latch=True)
 
    m = Joy()
-   m.axes = [ 0 ] * 18
-   m.buttons = [ 0 ] * 25
+   m.axes = [ 0 ] * 16
+   m.buttons = [ 0 ] * 35
    mode = None
 
    p = False
